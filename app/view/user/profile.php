3bd8788942c4758c4b6d758b64dfb491fba932cb
Adiyoga17 <adi.yoga04@gmail.com>
2021-11-03 12:10:14 +0800

    view

---
+++ app/view/user/profile.php
@@ -0,0 +1,12 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman</title>
+</head>
+<body>
+    <h1>Halaman User</h1>
+    <p>Halo, nama saya <?= $data['nama']; ?>, saya seorang <?= $data['pekerjaan'];?></p></p>
+</body>
+</html>
