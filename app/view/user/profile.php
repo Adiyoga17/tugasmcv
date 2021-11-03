8cec9fe70877cf40835c8041dda42166b734d814
Adiyoga17 <adi.yoga04@gmail.com>
2021-11-03 12:49:49 +0800

    asset

--- app/view/user/profile.php
+++ app/view/user/profile.php
@@ -1,12 +1,4 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Halaman</title>
-</head>
-<body>
-    <h1>Halaman User</h1>
-    <p>Halo, nama saya <?= $data['nama']; ?>, saya seorang <?= $data['pekerjaan'];?></p></p>
-</body>
-</html>
+<div class="container text-center mt-4">
+ <h1>Halaman User</h1><img src="<?= BASE_URL; ?>/img/profile.jpeg" class="rounded-circle shadow">
+ <p>Halo, nama saya <?= $data['nama']; ?>, saya seorang <?= $data['pekerjaan'];?></p>
+</div>
